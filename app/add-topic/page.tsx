f910841d7e491ffeb73d390b14b6ccecaae9dd4d
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-08-08 01:27:52 +0530

    chore: Refactor add-topic page layout

--- app/add-topic/page.tsx
+++ app/add-topic/page.tsx
@@ -6,21 +6,20 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-  <div className="p-6 w-full max-w-lg">
-    <div className="flex justify-center items-center">
-      <Input className="w-3/4" placeholder="Title" />
-    </div>
-    <div className="flex justify-center items-center mt-3">
-      <div className="flex-col w-2/3">
-        <Textarea placeholder="Type your message here." />
-        <div className="flex justify-center mt-4">
-          <Button>Add Topic</Button>
+      <div className="p-6 w-full max-w-lg">
+        <div className="flex justify-center items-center">
+          <Input className="w-3/4" placeholder="Title" />
+        </div>
+        <div className="flex justify-center items-center mt-3">
+          <div className="flex-col w-2/3">
+            <Textarea placeholder="Type your message here." />
+            <div className="flex justify-center mt-4">
+              <Button>Add Topic</Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
--- app/edit-topic/[id]/page.tsx
+++ app/edit-topic/[id]/page.tsx
@@ -1,8 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
-const page = (  ) => {
+const page = (params:any) => {
+
+  
+
   return (
-    <div>page</div>
+    <div>
+     <div className="flex justify-center items-center min-h-screen">
+      <div className="p-6 w-full max-w-lg">
+        <div className="flex justify-center items-center">
+          <Input className="w-3/4" placeholder="Title" />
+        </div>
+        <div className="flex justify-center items-center mt-3">
+          <div className="flex-col w-2/3">
+            <Textarea placeholder="Type your message here." />
+            <div className="flex justify-center mt-4">
+              <Button>Add Topic</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
